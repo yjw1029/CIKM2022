5ce278b173c2c239b62491e5432884b15806de3e
Jingwei Yi <v-jingweiyi@microsoft.com>
2022-08-30 02:41:31 +0000

    add FLRecoClient FLRecoRGNNClient

--- client/__init__.py
+++ client/__init__.py
@@ -1,5 +1,6 @@
 from .base import BaseClient
 from .rgnn import RGNNClient
+from .flreco import FLRecoClient, FLRecoRGNNClient
 
 def get_client_cls(client_cls):
     return eval(client_cls)
--- client/flreco.py
+++ client/flreco.py
@@ -7,7 +7,7 @@ class FLRecoClient(BaseClient):
     '''Refer to https://arxiv.org/abs/2102.03448.
      Federated Reconstruction: Partially Local Federated Learning.
     '''
-    def freeze_share_params(self):
+    def freeze_shared_params(self):
         # freeze global shared parameters
         for name, param in self.model.named_parameters():
             if not is_name_in_list(name, self.args.param_filter_list):
@@ -19,14 +19,113 @@ class FLRecoClient(BaseClient):
             param.requires_grad = True
 
     def reconstruct(self):
-        pass
+        self.freeze_shared_params()
 
+        step_cnt = 0
+        while step_cnt < self.args.reco_steps:
+            for data in self.dataloader_dict["train"]:
+                self.optimizer.zero_grad()
+                data = data.cuda()
+                pred = self.model(data)
+                if "classification" in self.task_type.lower():
+                    label = data.y.squeeze(-1).long()
+                elif "regression" in self.task_type.lower():
+                    label = data.y
+
+                if len(label.size()) == 0:
+                    label = label.unsqueeze(0)
+
+                loss = self.criterion(pred, label)
+
+                loss.backward()
+                self.optimizer.step()
+                step_cnt += 1
+        
+        self.unfreeze_all_params()
+
+        
     def train(self, reset_optim=True):
         if reset_optim:
             self.reset_optimizer()
 
         self.model = self.model.cuda()
         self.model.train()
+
+        self.reconstruct()
+
+        local_training_steps = 0
+        local_training_num = 0
+
+        for epoch in range(self.args.local_epoch):
+            for data in self.dataloader_dict["train"]:
+                self.optimizer.zero_grad()
+                data = data.cuda()
+                pred = self.model(data)
+                if "classification" in self.task_type.lower():
+                    label = data.y.squeeze(-1).long()
+                elif "regression" in self.task_type.lower():
+                    label = data.y
+
+                if len(label.size()) == 0:
+                    label = label.unsqueeze(0)
+
+                loss = self.criterion(pred, label)
+
+                loss.backward()
+                self.optimizer.step()
+
+                local_training_steps += 1
+                local_training_num += label.shape[0]
+
+        return local_training_num, local_training_steps
+
+
+class FLRecoRGNNClient(RGNNClient):
+    def freeze_shared_params(self):
+        # freeze global shared parameters
+        for name, param in self.model.named_parameters():
+            if not is_name_in_list(name, self.args.param_filter_list):
+                param.requires_grad = False
+
+    def unfreeze_all_params(self):
+        # unfreeze all parameters
+        for param in self.model.parameters():
+            param.requires_grad = True
+
+    def reconstruct(self):
+        self.freeze_shared_params()
+
+        step_cnt = 0
+        while step_cnt < self.args.reco_steps:
+            for data in self.dataloader_dict["train"]:
+                self.optimizer.zero_grad()
+                data = data.cuda()
+                pred = self.model(data)
+                if "classification" in self.task_type.lower():
+                    label = data.y.squeeze(-1).long()
+                elif "regression" in self.task_type.lower():
+                    label = data.y
+
+                if len(label.size()) == 0:
+                    label = label.unsqueeze(0)
+
+                loss = self.criterion(pred, label)
+
+                loss.backward()
+                self.optimizer.step()
+                step_cnt += 1
+        
+        self.unfreeze_all_params()
+
+        
+    def train(self, reset_optim=True):
+        if reset_optim:
+            self.reset_optimizer()
+
+        self.model = self.model.cuda()
+        self.model.train()
+
+        self.reconstruct()
 
         local_training_steps = 0
         local_training_num = 0
